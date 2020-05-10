7070fe55eaabf33b48c4036ab09674b1a772a64d
vivian1311 <elvis212332@gmail.com>
2020-05-10 17:14:47 +0800

    Add rpc_function.py

--- 9_demo/rpc_function.py
+++ 9_demo/rpc_function.py
@@ -3,38 +3,75 @@ import time
 serdev = '/dev/ttyACM0'
 s = serial.Serial(serdev)
 
-s.write(bytes("/LEDControl/run 2 0\r", 'UTF-8'))
+s.write(bytes("\r", 'UTF-8'))
 line=s.readline() # Read an echo string from K66F terminated with '\n' (pc.putc())
 print(line)
 line=s.readline() # Read an echo string from K66F terminated with '\n' (RPC reply)
 print(line)
-#time.sleep(1)
+time.sleep(1)
 
-while(True):
-    s.write(bytes("/LEDControl/run 1 0\r", 'UTF-8'))
-    line=s.readline() # Read an echo string from K66F terminated with '\n' (pc.putc())
-    print(line)
-    line=s.readline() # Read an echo string from K66F terminated with '\n' (RPC reply)
-    print(line)
-    time.sleep(1)
+s.write(bytes("/LEDControl/run 1 1\r", 'UTF-8'))
+line=s.readline() # Read an echo string from K66F terminated with '\n' (pc.putc())
+print(line)
+line=s.readline() # Read an echo string from K66F terminated with '\n' (RPC reply)
+print(line)
+time.sleep(1)
 
-    s.write(bytes("/LEDControl/run 1 1\r", 'UTF-8'))
-    line=s.readline() # Read an echo string from K66F terminated with '\n' (pc.putc())
-    print(line)
-    line=s.readline() # Read an echo string from K66F terminated with '\n' (RPC reply)
-    print(line)
-    #time.sleep(1)
+s.write(bytes("/LEDControl/run 2 1\r", 'UTF-8'))
+line=s.readline() # Read an echo string from K66F terminated with '\n' (pc.putc())
+print(line)
+line=s.readline() # Read an echo string from K66F terminated with '\n' (RPC reply)
+print(line)
+time.sleep(1)
 
-    s.write(bytes("/LEDControl/run 3 0\r", 'UTF-8'))
-    line=s.readline() # Read an echo string from K66F terminated with '\n' (pc.putc())
-    print(line)
-    line=s.readline() # Read an echo string from K66F terminated with '\n' (RPC reply)
-    print(line)
-    time.sleep(1)
+s.write(bytes("/LEDControl/run 3 1\r", 'UTF-8'))
 
-    s.write(bytes("/LEDControl/run 3 1\r", 'UTF-8'))
-    line=s.readline() # Read an echo string from K66F terminated with '\n' (pc.putc())
-    print(line)
-    line=s.readline() # Read an echo string from K66F terminated with '\n' (RPC reply)
-    print(line)
-    #time.sleep(1)
+line=s.readline() # Read an echo string from K66F terminated with '\n' (pc.putc())
+
+print(line)
+
+line=s.readline() # Read an echo string from K66F terminated with '\n' (RPC reply)
+
+print(line)
+
+time.sleep(1)
+
+
+s.write(bytes("/LEDControl/run 2 0\r", 'UTF-8'))
+
+line=s.readline() # Read an echo string from K66F terminated with '\n' (pc.putc())
+
+print(line)
+
+line=s.readline() # Read an echo string from K66F terminated with '\n' (RPC reply)
+
+print(line)
+
+time.sleep(1)
+
+
+s.write(bytes("/LEDControl/run 3 0\r", 'UTF-8'))
+
+line=s.readline() # Read an echo string from K66F terminated with '\n' (pc.putc())
+
+print(line)
+
+line=s.readline() # Read an echo string from K66F terminated with '\n' (RPC reply)
+
+print(line)
+
+time.sleep(1)
+
+
+s.write(bytes("/LEDControl/run 1 0\r", 'UTF-8'))
+
+line=s.readline() # Read an echo string from K66F terminated with '\n' (pc.putc())
+
+print(line)
+
+line=s.readline() # Read an echo string from K66F terminated with '\n' (RPC reply)
+
+print(line)
+
+
+s.close()
